59dcf92684e57deabb88a343c6bbf9b24039a1ff
Robert Fox <robertpaulfox@gmail.com>
2020-03-12 11:10:42 +0200

    updated shopdata with new data, updated cart utils in cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -33,11 +33,8 @@ const Header = ({ currentUser, hidden }) => (
             <CartIcon />
         </div>
         {   
-            hidden ? 
-            null 
-            :
-            <CartDropDown />
-            }
+            hidden ? null : <CartDropDown />
+        }
     </div>
 )
 
